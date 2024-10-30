2b134a3ae759c0d920d60d5e20bb72fbb41dbbbf
bernuly <bernuly@gmail.com>
2024-10-30 10:07:22 +0000

    Adding scene with UI

---
+++ Assets/Scripts/UIActions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIActions : MonoBehaviour
+{
+
+    public void Btn01() {
+        Debug.Log("UIActions::Btn01");
+    }
+
+    public void Btn02() {
+        Debug.Log("UIActions::Btn02");
+    }
+
+    public void Btn03() {
+        Debug.Log("UIActions::Btn03");
+    }
+
+    public void Btn04() {
+        Debug.Log("UIActions::Btn04");
+    }
+
+
+}
